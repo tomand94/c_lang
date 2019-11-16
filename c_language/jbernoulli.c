b064b81f219d3df41fdaf50eee1eedd07e946d30
tomand94 <tomand94@gmail.com>
2019-11-16 11:12:39 +0100

    poprawki

--- c_language/jbernoulli.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <math.h>
-
-// x^y
-long long int powerll (long long int x, long long int y);
-
-int main() {
-
-}
---
+++ c_language/jbernoulli.h
@@ -0,0 +1,6 @@
+// x^y
+long long int powerll (long long int x, long long int y);
+// 1^%d + 2^%d + ... + 1000^%d = %d
+void print_results (int x, int suma);
+//calculates 'suma'
+long long int calculate_suma(int y);
