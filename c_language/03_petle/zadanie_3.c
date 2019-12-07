91c7f885bc4dbab574f720ada374dd4255c6d92d
tomand94 <tomand94@gmail.com>
2019-12-07 09:38:31 +0100

    petle

--- c_language/03_petle/zadanie_3.c
+++ c_language/03_petle/zadanie_3.c
@@ -2,18 +2,27 @@
 
 #include <stdio.h>
 
-int main() {
-    int a = 0;
-    int b = 0;
+int a = 0;
+int b = 0;
+void scan_numbers();
+void print_numbers();
 
+int main() {
+    scan_numbers();
+    print_numbers();
+}
+
+void scan_numbers() {
     printf("Podaj liczbę a \n");
     scanf ("%d", &a);
 
     printf("Podaj liczbę b \n");
     scanf ("%d", &b);
 
-    printf("----------------------------\n");
+    printf("\n");
+}
 
+void print_numbers() {
     if (a<b) {
         while (a<=b) {
             printf("%d \n", a);
