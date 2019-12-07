c10ef96a98bac6b99195668c00ef292460b55c9f
tomand94 <tomand94@gmail.com>
2019-12-07 09:31:09 +0100

    petle

--- c_language/03_petle/zadanie_5.c
+++ c_language/03_petle/zadanie_5.c
@@ -2,15 +2,24 @@
 
 #include <stdio.h>
 #define SIZE 6
-int main(int argc, char const *argv[]) {
 
-    int tablica[SIZE];
+int tablica[SIZE];
+void print_table();
+void scan_table();
+
+int main() {
     printf("Podaj 6 liczb całkowitych do tablicy.\n");
+    scan_table();
+    print_table();
+}
 
-    for (int i = 0; i < SIZE; i++) {
+void scan_table() {
+  for (int i = 0; i < SIZE; i++) {
         scanf("%i", &tablica[i]);
     }
+}
 
+void print_table() {
     for (int i = 0 ; i < SIZE; i++) {
         printf ("Element numer %i = %i\n", (i+1), tablica[i]);
     }
--- c_language/03_petle/zadanie_6.c
+++ c_language/03_petle/zadanie_6.c
@@ -1,23 +1,66 @@
+// 6. Napisz program, który wczyta ze stdin 6 liczb rzeczywistych do tablicy, a następnie wyszuka i wypisze największą i najmniejszą liczbę umieszczoną w tej tablicy.
+
 #include <stdio.h>
-#include <stdlib.h>
+#define SIZE 6
 
-int main(int argc, char const *argv[])
-{
-  double max = atof(argv[1]);
-  double min = atof(argv[1]);
-  double number;
+int tablica[SIZE];
+void print_table();
+void scan_table();
+void biggest_number();
+void smallest_number();
 
-  for(int i = 2; i < argc; i++)
-  {
-    number = atof(argv[i]);
-    if(number < min)
-    {
-      min = number;
-    }
-    if (number > max)
-    {
-      max = number;
-    }
-  }
-  printf("Najwieksza liczba sposrod podanych to %lf\nNajmniejsza liczba sposrod podanych to %lf\n", max, min);
+int main() {
+    printf("Podaj 6 liczb całkowitych do tablicy.\n");
+    scan_table();
+    biggest_number();
+    smallest_number();
 }
+
+void scan_table() {
+  for (int i = 0; i < SIZE; i++) {
+        scanf("%i", &tablica[i]);
+    }
+}
+
+void print_table() {
+    for (int i = 0 ; i < SIZE; i++) {
+        printf ("Element numer %i = %i\n", (i+1), tablica[i]);
+    }
+}
+
+void biggest_number() {
+  for(int i = 0; i < SIZE; i++)
+    {
+       if(tablica[0] < tablica[i])
+           tablica[0] = tablica[i];
+    }
+    printf("Największa liczba: %d\n", tablica[0]);
+}
+
+void smallest_number() {
+  for(int i = 0; i < SIZE; i++)
+    {
+       if(tablica[0] > tablica[i])
+           tablica[0] = tablica[i];
+    }
+    printf("Najmniejsza liczba: %d\n", tablica[0]);
+}
+
+
+/* 
+void print_table() {
+    for (int i = 0 ; i < SIZE; i++) {
+        printf ("Element numer %i = %i\n", (i+1), tablica[i]);
+    }
+}
+
+void biggest_number() {
+  for(int i = 0; i < SIZE; i++)
+    {
+       // Change < to > if you want to find the smallest element
+       if(tablica[0] < tablica[i])
+           tablica[0] = tablica[i];
+    }
+    printf("Largest element = %d", tablica[0]);
+}
+*/
